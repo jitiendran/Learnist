d16c961298a2f850fcc8b9a0d0769d7705b19432
jitiendran <jitiendranjiji2000@gmail.com>
2021-01-01 16:20:07 +0530

    Adding login and register actions

--- assets/Learninst.js
+++ assets/Learninst.js
@@ -7,3 +7,18 @@ function removeloader(){
     document.getElementById("loading").style.display = "none";
     document.getElementById("web").style.display = "block";
 }
+function register(){
+    document.getElementById("login").style.display = "none";
+    document.getElementById("content").style.display = "none";
+    document.getElementById("register").style.display = "block";
+}
+function login(){
+    document.getElementById("content").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    document.getElementById("register").style.display = "none";
+}
+function back(){
+    document.getElementById("login").style.display = "none";
+    document.getElementById("content").style.display = "block";
+    document.getElementById("register").style.display = "none";
+}
