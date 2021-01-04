92f606a9499a5e9e76dc76167a0b814a872f711b
jitiendran <jitiendranjiji2000@gmail.com>
2021-01-04 16:59:35 +0530

    Added slideshow for courses

--- assets/Learninst.js
+++ assets/Learninst.js
@@ -29,5 +29,24 @@ function open_nav(){
 }
 function close_nav(){
     document.getElementById("nav-hidden").style.display = "none";
-
+}
+var i = 0;
+var slides = ["slide1","slide2","slide3","slide4","slide5","slide6"];
+function slide(){
+    var n = (i+4);
+    if(n < 6){
+        document.getElementById(slides[i]).style.display = "none";
+        document.getElementById(slides[n]).style.display = "block";
+        i++;
+    } 
+}
+function prev(){
+    console.log("The i now is : "+i);
+    var a = i-1;
+    var b = a+4;
+    if(a >= 0){
+        document.getElementById(slides[a]).style.display= "block";
+        document.getElementById(slides[b]).style.display = "none";
+        i--;
+    }  
 }
